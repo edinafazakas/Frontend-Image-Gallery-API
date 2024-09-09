4c316958d684d66374487595077d82df43071ff8
edinafazakas <fazakas_edina@yahoo.ro>
2024-09-09 20:11:51 +0300

    creating dropdown

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ async function fetchImagesJSON() {
 fetchImagesJSON().then(images => {
     const imageGrid = document.getElementById('imageGrid');
     images.forEach(image => {
+        console.log(image)
         const gridItem = document.createElement('div');
         gridItem.className = 'grid-item';
         gridItem.innerHTML = `
@@ -14,8 +15,23 @@ fetchImagesJSON().then(images => {
             <h2>${image.author}</h2>
             <p>Width: ${image.width}</p>
             <p>Height: ${image.height}</p>
-            <a href="${image.url}" target="_blank">View URL</a>
+            <a href="${image.url}">View URL</a>
         `;
         imageGrid.appendChild(gridItem);
+
+        const option = document.createElement('option');
+        option.value = image.download_url;
+        option.text = image.author;
+        imageDropdown.appendChild(option);
+    });
+
+    imageDropdown.addEventListener('change', function() {
+        const selectedImage = this.value;
+        const gridItems = document.querySelectorAll('.grid-item img');
+        gridItems.forEach(img => {
+            if (img.src === selectedImage) {
+                img.parentElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
     });
 });
