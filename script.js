0443ba46f18ed75a6b195fa9dfd515bb90cde7e5
edinafazakas <fazakas_edina@yahoo.ro>
2024-09-09 14:50:31 +0300

    creating grid

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 async function fetchImagesJSON() {
     const response = await fetch('https://picsum.photos/v2/list');
-    const image = await response.json();
-    return image;
-  }
-  
-  fetchImagesJSON().then(image => {
-    console.log(image);
-  });
+    const images = await response.json();
+    return images;
+}
+
+fetchImagesJSON().then(images => {
+    const imageGrid = document.getElementById('imageGrid');
+    images.forEach(image => {
+        const gridItem = document.createElement('div');
+        gridItem.className = 'grid-item';
+        gridItem.innerHTML = `
+            <img src="${image.download_url}" alt="${image.author}">
+            <h2>${image.author}</h2>
+            <p>Width: ${image.width}</p>
+            <p>Height: ${image.height}</p>
+            <a href="${image.url}" target="_blank">View URL</a>
+        `;
+        imageGrid.appendChild(gridItem);
+    });
+});
